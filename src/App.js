3c85a2edbafb0e7ce35fb697284eef196d0025e7
BULENT KAYICI <bulentkyc@MacBook-Pro.fritz.box>
2021-06-09 15:05:59 +0200

    v0.1

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import Auth from './pages/Auth';
+import Contacts from './pages/Contacts';
+import GetContact from './pages/GetContact';
+import Navbar from './components/Navbar';
+
+import { Switch,Route } from "react-router-dom";
+
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+    <Navbar/>
+    <Switch>
+      <Route path="/auth">
+        <Auth />
+      </Route>
+      <Route path="/contacts">
+        <Contacts />
+      </Route>
+      <Route path="/get-contact">
+        <GetContact />
+      </Route>
+      <Route path="/">
+        <Auth />
+      </Route>
+    </Switch>
+    </div>
+  );
+}
+
+export default App;
